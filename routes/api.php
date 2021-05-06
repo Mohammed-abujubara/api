d9635ed0d03bb9c25a8c29e40fcaeec0415cc78d
Moe Abujubara <abujubarah927@live.com>
2021-05-06 15:03:09 +0300

    Created Models and controllers

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiresource('/products', App\Http\Controllers\ProductController::class);
+Route::prefix('products')->group(function(){
+
+	Route::apiresource('/{product}/reviews', App\Http\Controllers\ReviewController::class);
+});
