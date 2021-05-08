6bf844105bc62d81a2869b30b0181aa0b5c3a12a
Moe Abujubara <abujubarah927@live.com>
2021-05-08 07:15:58 +0300

    Product update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -14,6 +16,12 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct(){
+
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
     public function index()
     {
         return ProductCollection::collection(Product::paginate(20));    
@@ -35,9 +43,25 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         //
+
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+
+        $product->save();
+
+        return response([
+
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+
     }
 
     /**
@@ -73,6 +97,13 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         //
+
+        $product->update($request->all());
+        
+        return response([
+
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -84,5 +115,6 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+
     }
 }
